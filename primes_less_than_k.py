59b62cf7071a03bd82892d560b78db588670fd89
Sunkara Alekhya Sai <98542324+21A91A04G6@users.noreply.github.com>
2023-07-08 10:15:45 +0530

    def prime(a):     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 a=int(input()) l=list(map(int,input().split())) b=int(input()) c=0 for n in l:     if prime(n)==1 and  n!=1 and n<=b:         c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,13 @@
+def prime(a):
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return 0
+    return 1
+a=int(input())
+l=list(map(int,input().split()))
+b=int(input())
+c=0
+for n in l:
+    if prime(n)==1 and  n!=1 and n<=b:
+        c+=1
+print(c)
