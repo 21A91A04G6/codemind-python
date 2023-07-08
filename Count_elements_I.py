01cfdf16aff7140edbe44bc6981bd037ade6f6d5
Sunkara Alekhya Sai <98542324+21A91A04G6@users.noreply.github.com>
2023-07-08 10:53:50 +0530

    a,b=map(int,input().split()) n=list(map(int,input().split())) m=list(map(int,input().split())) c=[] d=[] e=[] for i in n:     if i in m:         if i not in c:             c.append(i) for i in m:     if i  in n:         if i not in c:             c.append(i) print(len(c))

---
+++ Count_elements_I.py
@@ -0,0 +1,15 @@
+a,b=map(int,input().split())
+n=list(map(int,input().split()))
+m=list(map(int,input().split()))
+c=[]
+d=[]
+e=[]
+for i in n:
+    if i in m:
+        if i not in c:
+            c.append(i)
+for i in m:
+    if i  in n:
+        if i not in c:
+            c.append(i)
+print(len(c))
