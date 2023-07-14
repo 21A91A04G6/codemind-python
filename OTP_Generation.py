30528e6f6f88e53130ba9964b0c3b2b6430a6936
Sunkara Alekhya Sai <98542324+21A91A04G6@users.noreply.github.com>
2023-07-14 12:09:25 +0530

    n=input() k=list(n) for i in k:     if int(i)%2!=0:         print(int(i)**2,end="")

--- OTP_Generation.py
+++ OTP_Generation.py
@@ -1,7 +1,5 @@
 n=input()
-l=list(n)
-a=[]
-for i in range(len(l)):
-    if (int(l[i])%2)!=0:
-        a.append(str(int(l[i])*int(l[i])))
-print(''.join(a))
+k=list(n)
+for i in k:
+    if int(i)%2!=0:
+        print(int(i)**2,end="")
